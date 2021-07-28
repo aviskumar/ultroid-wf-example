0798bb2cdfbe866ae65241b9d2f9fa3a9e6849c5
Avish Kumar <85635883+aviskumar@users.noreply.github.com>
2021-07-28 22:41:51 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,6 +1,6 @@
-branch=dev
-git clone -b $branch https://github.com/TeamUltroid/Ultroid /root/TeamUltroid
-cp ultroid/.env /root/TeamUltroid/.env
+branch=main
+git clone -b $branch https://github.com/aviskumar/Ultroid-Sudo /root/TeamUltroid
+cp ultroid-sudo/.env /root/TeamUltroid/.env
 cd /root/TeamUltroid
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
 docker run --privileged --env-file .env --rm -i ultroid
