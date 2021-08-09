2fa7b26fbe97342e5628f20b5fda3494ade24a49
Avish Kumar <85635883+aviskumar@users.noreply.github.com>
2021-08-09 10:35:56 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -2,5 +2,6 @@ branch=dev
 git clone -b $branch https://github.com/TeamUltroid/Ultroid /root/TeamUltroid
 cp ultroid/.env /root/TeamUltroid/.env
 cd /root/TeamUltroid
+pip install ffmpeg-python
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
 docker run --privileged --env-file .env --rm -i ultroid
